c168d80c4ff97be2b1ca64d245bfb71065aeecd8
Crystel Montero <crysvane05@estudiantec.cr>
2021-09-24 01:11:26 -0600

    Interfaz

--- src/VIEW/Main.java
+++ src/VIEW/Main.java
@@ -6,13 +6,14 @@ import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
 
+import java.util.Objects;
+
 public class Main extends Application {
 
     @Override
     public void start(Stage primaryStage) throws Exception{
-        Parent root = FXMLLoader.load(getClass().getResource("sample.fxml"));
-        primaryStage.setTitle("Hola Crystel jeje stas bien wapa");
-        primaryStage.setScene(new Scene(root, 450, 275));
+        Parent root = FXMLLoader.load(Objects.requireNonNull(getClass().getResource("VentanaPrincipal.fxml")));
+        primaryStage.setScene(new Scene(root));
         primaryStage.show();
     }
 
